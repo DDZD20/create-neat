5b2716998f1dcb2c0e9798b6d2e532480748e73e
DDZD20 <2353880192@qq.com>
2025-03-12 22:09:02 +0800

    test: ✅ utils单测补充，单测覆盖率待提升

--- packages/core/vitest.config.ts
+++ packages/core/vitest.config.ts
@@ -6,6 +6,10 @@ export default defineConfig({
   test: {
     globals: true,
     environment: "node",
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "json", "html"],
+    },
   },
   resolve: {
     alias: {
